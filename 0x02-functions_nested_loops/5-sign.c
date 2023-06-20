1b28c4ce04c9bc68bfc9510a49518d366b9c16fd
ismail24lkkk <smailfatimi72@gmail.com>
2023-06-20 08:49:00 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,8 @@
 7 * Return: 1 and prints + if n is greater than zero
 8 *         0 and prints 0 if n is zero
 9 *        -1 and prints - if n is less than zero
-10 *int print_sign(int n)
+10 */
+int print_sign(int n)
 11{
 12    if (n > 0)
 13    {
@@ -17,12 +18,12 @@
 17    else if (n == 0)
 18    {
 19        _putchar('0');
-20        return 0;
-21    }
-22    else
-23    {
-24        _putchar('-');
-25        return -1;
-26    }
-27}
-28
+19        return 0;
+20    }
+21    else
+22    {
+23        _putchar('-');
+24        return -1;
+25    }
+26}
+27
