554e33b42afc4c1a658681aa861771f576d78f76
ismail24lkkk <smailfatimi72@gmail.com>
2023-07-19 16:04:48 -0700

    ne

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,75 +1,29 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
  *
- * Return: ...
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
 }
 
-/**
- * op_sub - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
- * op_mul - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a / b);
-}
-
-/**
- * op_mod - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a % b);
-}
