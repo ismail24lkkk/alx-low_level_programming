2cd7a0945711188c30e261def296561415324ea4
ismail24lkkk <134705795+ismail24lkkk@users.noreply.github.com>
2023-08-08 01:42:49 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,29 +3,35 @@
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
  * @letters: number of letters to read and print
- * Return: the number of letters printed
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+	int i, y;
 	char *buf;
-	size_t r, w;
-
-	if (filename == NULL || letters < 1)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
-	buf = malloc(letters);
-	if (buf == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
-	r = read(fd, buf, letters);
-	if (r < 1)
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
 		return (0);
-	w = write(STDOUT_FILENO, buf, r);
-	if (w < 1)
-		return (0);
-	free(buf);
+	}
+	buf[i] = '\0';
 	close(fd);
-	return (w);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
