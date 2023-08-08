4a76b4820e3b014533d40a9aeaa546cfa8ed40e8
ismail24lkkk <smailfatimi72@gmail.com>
2023-08-07 18:35:50 -0700

    mliha

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * read_textfile - reads a text file and prints it to the standard output
@@ -36,42 +35,3 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buf);
 	return (y);
 }
-=======
-#include "main.h"
-/**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	buf[i] = '\0';
-	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (y);
-}
->>>>>>> 2cd7a0945711188c30e261def296561415324ea4
